16c0f6f845572c2d636649adbccd24a593662412
UsamaBlavins <N0680788@my.ntu.ac.uk>
2017-10-24 00:05:49 +0100

    declared variable in namespace

--- SDILab3/card.cpp
+++ SDILab3/card.cpp
@@ -9,8 +9,15 @@
 
 
 namespace SDI_REVIEW {
-    const std::vector<std::string> Card::faceNames = { "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King" };
+    
+    const std::vector<std::string> Card::faceNames = {
+        "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"
+    };
     //this is the second task
+    const std::vector<std::string> suitNames = {
+        "Hearts", "Clubs", "Diamonds", "Spades"
+    };
+    
     
     Card::Card(int cardNum) {
         assert(cardNum < 52);
@@ -27,7 +34,6 @@ namespace SDI_REVIEW {
     }
     
     std::string Card::suitName(int suitNum) {
-        const std::vector<std::string> suitNames = { "Hearts", "Clubs", "Diamonds", "Spades" };
         assert(suitNum < 4);
         return (suitNames[suitNum]);
     }
