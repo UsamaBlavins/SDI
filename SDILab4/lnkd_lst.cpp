c95c7a158a418e2d68d383d0b34c0fc2eaa4d987
UsamaBlavins <N0680788@my.ntu.ac.uk>
2017-10-29 20:29:49 +0000

    first traverses the entire list

---
+++ SDILab4/lnkd_lst.cpp
@@ -0,0 +1,48 @@
+
+#include <iostream>
+
+struct node {
+ int data;
+ node* next;
+};
+
+int main() {
+ node *list, *first, *current, *last, *temp, *trail, *p, *q;
+ list = new node;
+ list->data = 47;
+ temp = list;
+ first = list;
+ list = new node;
+ list->data = 12;
+ temp->next = list;
+ temp = temp->next;
+ list = new node;
+ list->data = 92;
+ temp->next = list;
+ temp = temp->next;
+ list = new node;
+ list->data = 65;
+ current = list;
+ temp->next = list;
+ temp = temp->next;
+ list = new node;
+ list->data = 80;
+ temp->next = list;
+ temp = temp->next;
+ list = new node;
+ list->data = 46;
+ temp->next = list;
+ temp = temp->next;
+ list = new node;
+ list->data = 5;
+ temp->next = list;
+ temp = temp->next;
+ list = new node;
+ list->data = 78;
+ last = list;
+ temp->next = list;
+ temp = temp->next;
+ list->next = NULL;
+ temp->next = list->next;
+ return 0;
+}
